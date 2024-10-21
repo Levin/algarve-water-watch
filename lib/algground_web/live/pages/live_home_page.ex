b0d25611e56bfc4dff31308961ef5e2b9992fc84
levin <mail.an.levin@gmail.com>
2024-10-21 20:36:56 +0000

    modals

--- lib/algground_web/live/pages/live_home_page.ex
+++ lib/algground_web/live/pages/live_home_page.ex
@@ -151,24 +151,53 @@ defmodule AlggroundWeb.LiveHomePage do
           <p class="mx-auto max-w-lg text-pretty text-center text-4xl font-medium tracking-tight sm:text-3xl ">
             <%= display_groundwater(get_region(@regions, "Albufeira")) %>
           </p>
-          <p class="mx-auto max-w-lg text-pretty text-center font-sm tracking-tight text-gray-400 text-sm cursor-pointer">
-            Ground Water Level
-          </p>
+          <div class="flex justify-left ">
+            <p class="flex gap-2 mx-auto text-pretty font-sm tracking-tight text-gray-400 text-sm cursor-pointer">
+              Ground Water Level
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 320 512"
+                class="h-6 w-6 left-2.5 top-2 border rounded-full bg-gray-200 p-1"
+                phx-click={show_modal("frame_groundwater")}
+              >
+                <path d="M80 160c0-35.3 28.7-64 64-64l32 0c35.3 0 64 28.7 64 64l0 3.6c0 21.8-11.1 42.1-29.4 53.8l-42.2 27.1c-25.2 16.2-40.4 44.1-40.4 74l0 1.4c0 17.7 14.3 32 32 32s32-14.3 32-32l0-1.4c0-8.2 4.2-15.8 11-20.2l42.2-27.1c36.6-23.6 58.8-64.1 58.8-107.7l0-3.6c0-70.7-57.3-128-128-128l-32 0C73.3 32 16 89.3 16 160c0 17.7 14.3 32 32 32s32-14.3 32-32zm80 320a40 40 0 1 0 0-80 40 40 0 1 0 0 80z" />
+              </svg>
+            </p>
+          </div>
           <div class="mt-10 grid gap-4 sm:mt-4 lg:rounded-t-[2rem]">
             <div class="relative mb-4">
               <div class="relative flex h-full flex-col overflow-hidden rounded-[calc(2rem+1px)]">
+                <.modal id="frame_groundwater"></.modal>
+                <.modal id="frame_rainfall"></.modal>
+                <.modal id="frame_reservoir"></.modal>
                 <div class="flex justify-evenly mb-8">
                   <div class="px-8 pt-8 sm:px-10 sm:pt-10">
-                    <p class="mt-2 text-md font-medium tracking-tight text-gray-950 max-lg:text-center cursor-pointer">
+                    <p class="flex gap-2 mt-2 text-md font-medium tracking-tight text-gray-950 max-lg:text-center cursor-pointer ">
                       Rainfall
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 320 512"
+                        class="h-6 w-6 left-2.5 top-2 border rounded-full bg-gray-200 p-1"
+                        phx-click={show_modal("frame_rainfall")}
+                      >
+                        <path d="M80 160c0-35.3 28.7-64 64-64l32 0c35.3 0 64 28.7 64 64l0 3.6c0 21.8-11.1 42.1-29.4 53.8l-42.2 27.1c-25.2 16.2-40.4 44.1-40.4 74l0 1.4c0 17.7 14.3 32 32 32s32-14.3 32-32l0-1.4c0-8.2 4.2-15.8 11-20.2l42.2-27.1c36.6-23.6 58.8-64.1 58.8-107.7l0-3.6c0-70.7-57.3-128-128-128l-32 0C73.3 32 16 89.3 16 160c0 17.7 14.3 32 32 32s32-14.3 32-32zm80 320a40 40 0 1 0 0-80 40 40 0 1 0 0 80z" />
+                      </svg>
                     </p>
                     <p class="mt-2 max-w-lg text-lg/6 text-gray-600 max-lg:text-center">
                       <%= display_rainfall(get_region(@regions, "Albufeira")) %>
                     </p>
                   </div>
                   <div class="px-8 pt-8 sm:px-10 sm:pt-10">
-                    <p class="mt-2 text-md font-medium tracking-tight text-gray-950 max-lg:text-center cursor-pointer">
+                    <p class="mt-2 text-md font-medium tracking-tight text-gray-950 max-lg:text-center cursor-pointer flex gap-2">
                       Reservoirs
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 320 512"
+                        class="h-6 w-6 left-2.5 top-2 border rounded-full bg-gray-200 p-1"
+                        phx-click={show_modal("frame_reservoir")}
+                      >
+                        <path d="M80 160c0-35.3 28.7-64 64-64l32 0c35.3 0 64 28.7 64 64l0 3.6c0 21.8-11.1 42.1-29.4 53.8l-42.2 27.1c-25.2 16.2-40.4 44.1-40.4 74l0 1.4c0 17.7 14.3 32 32 32s32-14.3 32-32l0-1.4c0-8.2 4.2-15.8 11-20.2l42.2-27.1c36.6-23.6 58.8-64.1 58.8-107.7l0-3.6c0-70.7-57.3-128-128-128l-32 0C73.3 32 16 89.3 16 160c0 17.7 14.3 32 32 32s32-14.3 32-32zm80 320a40 40 0 1 0 0-80 40 40 0 1 0 0 80z" />
+                      </svg>
                     </p>
                     <p class="mt-2 max-w-lg text-lg/6 text-gray-600 max-lg:text-center">
                       <%= display_reservoirs(get_region(@regions, "Albufeira")) %>
